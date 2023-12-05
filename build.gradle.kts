8c93e03f9a194c8d348931931be5e371d2685a7d
soonhankwon <soonable@gmail.com>
2023-12-05 18:40:35 +0900

    build: Java21 -> 17(#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ group = "dev"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -33,7 +33,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "21"
+        jvmTarget = "17"
     }
 }
 
